40d56a45b92f7ea67db282794637bb9ae5151bd2
Danik616 <ddanielvargas2011@gmail.com>
2023-06-28 11:46:10 -0500

    Se configura el acceso a la base de datos

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import httpServer from "#Config/http.ts"
 import '#Config/env.ts'
+import connectDB from "#Config/db.ts"
 
 const bootstrap= async ()=>{
+
+    await connectDB(String(process.env.MONGODB_URL))
 
     httpServer.listen(process.env.PORT, () =>{
         console.log(`Servidor escuchando en el puerto ${process.env.PORT}`)

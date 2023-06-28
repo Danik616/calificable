f6f24be9f1e30a36582484fc1d0b4c6df5b2f329
Danik616 <ddanielvargas2011@gmail.com>
2023-06-28 09:54:50 -0500

    Se hace el esqueleto inicial

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+const app= express()
+app.use(express.json())
+
+const PORT= 3000
+
+
+app.get('/ping', (req, res) => {
+    console.log("logrado")
+    res.send('pong')
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor en el puerto ${PORT}`)
+})
